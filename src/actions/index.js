f86cb6ba9d8818fe90152d06ab6e09ce088dc7c1
Nick Trandafil <Nick@66.0.168.192.in-addr.arpa>
2017-04-22 02:11:48 +0300

    Implement weather react app

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+const API_KEY = '1c2d62cf3b1634227eaa513ea0acf63a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
+
---
+++ src/containers/weather_list.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import SparkComponent from '../components/spark_component';
+
+
+
+ class WeatherList extends Component {
+
+renderWeather(cityWeather) {
+    const name = cityWeather.city.name;
+    const temps = cityWeather.list.map(weather => weather.main.temp);
+    const pressure = cityWeather.list.map(weather => weather.main.pressure);
+    const humidity = cityWeather.list.map(weather => weather.main.humidity);  
+    return (
+        <tr key={name} >
+            <td>{ name }</td>
+            <td>
+               <SparkComponent data={temps} color="blue" units="K" />
+            </td>
+             <td>
+               <SparkComponent data={pressure} color="green" units="hPa" />
+            </td>
+              <td>
+               <SparkComponent data={humidity} color="red" units="%" />
+            </td>
+        </tr>
+    )
+}
+
+
+
+    render() {
+        return (
+            <table className="table table-hover" >
+                <thread>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature K </th>
+                        <th>Pressure hPa </th>
+                        <th>Humidity % </th>
+                    </tr>
+                </thread>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return {weather};
+}
+
+export default connect(mapStateToProps,)(WeatherList);
